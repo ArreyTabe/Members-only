b8b6c98aa68216c61cc5a59002e40d8b62481a19
Arrey Tabe <arrey2391@gmail.com>
2021-05-31 13:13:20 +0100

    add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,6 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(params[:post].permit(:title, :text))
-
-
         @post.save 
         redirect_to @post
     end
